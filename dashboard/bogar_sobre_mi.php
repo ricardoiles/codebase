eef5487f1f034851c9612394950a981a377d82ea
Ricardo Iles <30296991+ricardoiles@users.noreply.github.com>
2023-02-10 18:00:04 -0500

    sobre mi al 30%

--- dashboard/bogar_sobre_mi.php
+++ dashboard/bogar_sobre_mi.php
@@ -11,6 +11,8 @@
 $cb->l_header_style = 'dark-glass';
 ?>
 
+<?php $cb->get_css('js/plugins/simplemde/simplemde.min.css'); ?>
+
 <!-- Page JS Plugins CSS -->
 <?php $cb->get_css('js/plugins/magnific-popup/magnific-popup.css'); ?>
 
@@ -65,47 +67,115 @@ $cb->l_header_style = 'dark-glass';
 <!-- Page Content -->
 <div class="content">
  <!-- Settings -->
-  <h2 class="content-heading"><i class="fa fa-user-circle me-1"></i> Sobre mi</h2>
+  <h2 class="content-heading" id="sobremi"><i class="fa fa-user-circle me-1"></i> Sobre mi</h2>
   <div class="block block-rounded">
     <div class="block-content">
       <div class="row items-push">
+        <?php
+          $data = $connect->query("SELECT * FROM sobre_mi")->fetchAll();
+          foreach ($data as $row) {
+        ?>
         <div class="col-lg-6 offset-lg-1">
           <span class="fs-2 fw-bold">¿Quien es César Nuñez?</span> <br>
-          <small>Músico/baterista interprete con más de 15 años de experiencia.</small>
-          <hr width="50">
-          <p class="text-muted">
-            Soy un enamorado de la música y sus ritmos. Alguien apasionado por ayudar y compartir mis conocimientos y habilidades. Proporciono un servicio profesional, de calidad, siempre aplicando mis conocimientos y experiencia, cumpliendo con ética, responsabilidad, compromiso y propósito a la realización de las metas y objetivos que surgen de cada proyecto.
-          </p>
-          <a class="btn rounded-pill btn-alt-primary mb-2" href="javascript:void(0)">
-            <i class="fa fa-arrow-down me-1"></i> Conoce mi historia
-          </a>
+          <form action="php/sobremiController.php" method="post" name="update_info_sobre_mi">
+            <small>
+              <input type="text" class="form-control" value="<?php echo $row['subtitulo'];?>" 
+              placeholder="<?php echo $row['subtitulo'];?>" name="subtitulo" required>
+            </small>
+            <hr width="50">
+              <p class="">
+                <label class="form-label" for="example-textarea-input">Descripción:</label>
+                <textarea class="form-control" id="example-textarea-input" name="descripcion" 
+                  rows="7" required
+                  placeholder="Descripción sobre ti.."><?php echo $row['descripcion'];?></textarea>
+              </p>
+              <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary me-1" name="update_info_sobre_mi">
+                  <i class="fa-solid fa-floppy-disk"></i>
+                  Actualizar
+                </button>
+              </div>
+          </form>
+            <p class="text-center">- O -</p>
+            <!-- foto de portada-->
+            <form action="php/sobremiController.php" method="post" name="update_foto_sobre_mi"
+              enctype="multipart/form-data">
+              <div class="mb-3">
+                <label class="form-label" for="example-text-input">Foto:</label>
+                <div class="input-group">
+                  <input type="file" class="form-control" id="inputGroupFile04" 
+                    aria-describedby="inputGroupFileAddon04" aria-label="Upload"
+                    name="foto" accept="image/*">
+                    <button class="btn btn-primary" type="submit" id="inputGroupFileAddon04"
+                      name="update_foto_sobre_mi">
+                      <i class="fa-solid fa-floppy-disk"></i>
+                      Actualizar
+                    </button>
+                </div>
+              </div>
+            </form>
         </div>
         <div class="col-lg-4">
             <div class="text-center">
-                <img class="img-fluid rounded" src="http://admin.cesarnunezonline.com/img/upload_images/CNOnline%20Instrumento1(about2).png" class="rounded" alt="...">
+              <a href="assets/media/sobre_mi/<?php echo $row['foto'];?>" download>
+                <img class="img-fluid rounded" src="assets/media/sobre_mi/<?php echo $row['foto'];?>" class="rounded">
+              </a>
             </div>
         </div>
+        <?php }?>
       </div>
     </div>
   </div>
+
   <div class="block block-rounded">
-    <div class="block-content" id="cesarnunez">
+    <div class="block-content" id="mihistoria">
       <div class="row items-push">
+        <?php
+          $data = $connect->query("SELECT * FROM mi_historia")->fetchAll();
+          foreach ($data as $row) {
+        ?>
         <div class="col-lg-3">
           <div class="text-center">
-            <img class="img-fluid rounded" src="http://admin.cesarnunezonline.com/img/upload_images/Cesar%20Nunez%20Online%20INdex.png" class="rounded" alt="...">
+            <img class="img-fluid rounded" src="assets/media/mi_historia/<?php echo $row['foto'];?>" class="rounded">
           </div>
         </div>
         <div class="col-lg-7 offset-lg-1">
           <span class="fs-2 fw-bold">Conoce mi historia</span>
           <hr width="50">
-          <p class="text-muted">
-            Soy un enamorado de la música y sus ritmos. Alguien apasionado por ayudar y compartir mis conocimientos y habilidades. Proporciono un servicio profesional, de calidad, siempre aplicando mis conocimientos y experiencia, cumpliendo con ética, responsabilidad, compromiso y propósito a la realización de las metas y objetivos que surgen de cada proyecto.
-          </p>
-          <a class="btn rounded-pill btn-alt-primary mb-2" href="javascript:void(0)">
-            <i class="fa fa-arrow-down me-1"></i> Conoce mi historia
-          </a>
+            <!-- foto mi_historia-->
+            <form action="php/sobremiController.php" method="post" name="update_foto_historia"
+              enctype="multipart/form-data">
+              <div class="mb-3">
+                <label class="form-label" for="example-text-input">Foto:</label>
+                <div class="input-group">
+                  <input type="file" class="form-control" id="inputGroupFile04" 
+                    aria-describedby="inputGroupFileAddon04" aria-label="Upload"
+                    name="foto" accept="image/*">
+                    <button class="btn btn-primary" type="submit" id="inputGroupFileAddon04"
+                      name="update_foto_historia">
+                      <i class="fa-solid fa-floppy-disk"></i>
+                      Actualizar
+                    </button>
+                </div>
+              </div>
+            </form>
+            <p class="text-center">- O -</p>
+          <div class="block" style="height: 3%!important">
+            <form action="php/sobremiController.php" method="post" name="update_mi_historia"
+              enctype="multipart/form-data">
+              <label class="form-label" for="example-text-input">Descripción:</label>
+              <textarea class="js-simplemde" id="simplemde" name="descripcion"
+              ><?php echo $row['descripcion'];?></textarea>
+              <div class="d-grid gap-2">
+                <button type="submit" class="btn btn-primary me-1" name="update_mi_historia">
+                  <i class="fa-solid fa-floppy-disk"></i>
+                  Actualizar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
+      <?php }?>
       </div>
     </div>
   </div>
@@ -116,166 +186,88 @@ $cb->l_header_style = 'dark-glass';
     Trayectorias y experiencias
 </h2>
   <div class="row items-push">
-    <div class="col-md-6 col-xl-3">
-      <!-- Story #1 -->
-      <div class="block block-rounded d-flex flex-column h-100 mb-0">
-        <div class="block-content block-content-full bg-image flex-grow-0" style="height: 180px; background-image: url('<?php echo $cb->assets_folder; ?>/media/photos/photo43.jpg');">
-          <span class="badge bg-black-50 fw-bold p-2 text-uppercase">
-            Business
-          </span>
-        </div>
-        <div class="block-content flex-grow-1">
-          <h5 class="mb-1">
-            <a class="text-dark" href="javascript:void(0)">
-              Starting your own home based online business from scratch
-            </a>
-          </h5>
-          <p class="fw-medium fs-sm text-muted">
-            <?php echo $cb->get_name(); ?> &middot; 24 min
-          </p>
-        </div>
-        <div class="block-content py-3 bg-body-light flex-grow-0">
-          <div class="row g-0 fs-sm text-center">
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="far fa-fw fa-eye opacity-50 me-1"></i> 1.5k
+    <p><span class="text-mute">Giras dentro de la República Mexicana y el extranjero.</span></p>
+    <?php
+      $data = $connect->query("SELECT * FROM expe_traye")->fetchAll();
+      foreach ($data as $row) {
+    ?>
+      <div class="col-md-6 col-xl-4">
+        <!-- Story #1 -->
+        <div class="block block-rounded d-flex flex-column h-100 mb-0">
+          <div class="block-content block-content-full bg-image flex-grow-0" 
+            style="height: 180px; background-image: 
+              url('assets/media/expe_traye/<?php echo $row['imagen'];?>');">
+            <span class="badge bg-black-50 fw-bold p-2 text-uppercase">
+              <?php echo $row['tipo'];?>
+            </span>
+          </div>
+          <div class="block-content flex-grow-1">
+            <h5 class="mb-1">
+              <a class="text-dark" href="javascript:void(0)">
+                <?php echo $row['titulo'];?>
               </a>
+            </h5>
+            <p class="fw-medium fs-sm text-muted">
+              <?php echo $row['descripcion'];?>
+            </p>
+            <p class="fw-medium fs-sm text-muted">
+              <i class="fa-solid fa-fw fa-location-dot"></i>
+              <?php echo $row['lugar'];?> 
+              &middot; 
+              <i class="fa-regular fa-fw fa-calendar me-1"></i>
+              <?php echo $row['fecha'];?>
+            </p>
+            <div class="d-grid gap-2">
+              <button type="submit" class="btn btn-primary me-1" name="update_mi_historia">
+                <i class="fa-solid fa-floppy-disk"></i>
+                Actualizar
+              </button>
             </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-heart opacity-50 me-1"></i> 300
-              </a>
-            </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-comments opacity-50 me-1"></i> 750
-              </a>
+            <br>
+            <form action="php/sobremiController.php" method="post" name="update_foto_historia"
+                enctype="multipart/form-data">
+                <div class="mb-3">
+                  <p class="text-center">- O -</p>
+                  <label class="form-label" for="example-text-input">
+                    Imagen: (Imagen actual: <a class="text-primary" 
+                      href="assets/media/expe_traye/<?php echo $row['imagen'];?>" 
+                      download>
+                      <?php echo $row['imagen'];?>
+                      <i class="fa-regular fa-arrow-down-to-line"></i>
+                    </a> )
+                  </label>
+                  <input type="file" class="form-control" id="inputGroupFile04" 
+                    aria-describedby="inputGroupFileAddon04" aria-label="Upload"
+                    name="foto" accept="image/*">
+                </div>
+                <div class="mb-3">
+                  <div class="d-grid gap-2">
+                    <button type="submit" class="btn btn-primary me-1" name="update_mi_historia">
+                      <i class="fa-solid fa-floppy-disk"></i>
+                      Actualizar
+                    </button>
+                  </div>
+                </div>
+              </form>
+          </div>
+          <div class="block-content py-3 bg-body-light flex-grow-0">
+            <div class="row g-0 fs-sm text-center">
+              <div class="col-6">
+                <a class="text-muted fw-semibold" href="javascript:void(0)">
+                  <i class="far fa-fw fa-edit me-1"></i> Editar
+                </a>
+              </div>
+              <div class="col-6">
+                <a class="fw-semibold text-warning" href="javascript:void(0)">
+                  <i class="fa fa-fw fa-trash me-1"></i> Eliminar
+                </a>
+              </div>
             </div>
           </div>
         </div>
+        <!-- END Story #1 -->
       </div>
-      <!-- END Story #1 -->
-    </div>
-    <div class="col-md-6 col-xl-3">
-      <!-- Story #1 -->
-      <div class="block block-rounded d-flex flex-column h-100 mb-0">
-        <div class="block-content block-content-full bg-image flex-grow-0" style="height: 180px; background-image: url('<?php echo $cb->assets_folder; ?>/media/photos/photo43.jpg');">
-          <span class="badge bg-black-50 fw-bold p-2 text-uppercase">
-            Business
-          </span>
-        </div>
-        <div class="block-content flex-grow-1">
-          <h5 class="mb-1">
-            <a class="text-dark" href="javascript:void(0)">
-              Starting your own home based online business from scratch
-            </a>
-          </h5>
-          <p class="fw-medium fs-sm text-muted">
-            <?php echo $cb->get_name(); ?> &middot; 24 min
-          </p>
-        </div>
-        <div class="block-content py-3 bg-body-light flex-grow-0">
-          <div class="row g-0 fs-sm text-center">
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="far fa-fw fa-eye opacity-50 me-1"></i> 1.5k
-              </a>
-            </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-heart opacity-50 me-1"></i> 300
-              </a>
-            </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-comments opacity-50 me-1"></i> 750
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- END Story #1 -->
-    </div>
-    <div class="col-md-6 col-xl-3">
-      <!-- Story #1 -->
-      <div class="block block-rounded d-flex flex-column h-100 mb-0">
-        <div class="block-content block-content-full bg-image flex-grow-0" style="height: 180px; background-image: url('<?php echo $cb->assets_folder; ?>/media/photos/photo43.jpg');">
-          <span class="badge bg-black-50 fw-bold p-2 text-uppercase">
-            Business
-          </span>
-        </div>
-        <div class="block-content flex-grow-1">
-          <h5 class="mb-1">
-            <a class="text-dark" href="javascript:void(0)">
-              Starting your own home based online business from scratch
-            </a>
-          </h5>
-          <p class="fw-medium fs-sm text-muted">
-            <?php echo $cb->get_name(); ?> &middot; 24 min
-          </p>
-        </div>
-        <div class="block-content py-3 bg-body-light flex-grow-0">
-          <div class="row g-0 fs-sm text-center">
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="far fa-fw fa-eye opacity-50 me-1"></i> 1.5k
-              </a>
-            </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-heart opacity-50 me-1"></i> 300
-              </a>
-            </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-comments opacity-50 me-1"></i> 750
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- END Story #1 -->
-    </div>
-     <div class="col-md-6 col-xl-3">
-      <!-- Story #1 -->
-      <div class="block block-rounded d-flex flex-column h-100 mb-0">
-        <div class="block-content block-content-full bg-image flex-grow-0" style="height: 180px; background-image: url('<?php echo $cb->assets_folder; ?>/media/photos/photo43.jpg');">
-          <span class="badge bg-black-50 fw-bold p-2 text-uppercase">
-            Business
-          </span>
-        </div>
-        <div class="block-content flex-grow-1">
-          <h5 class="mb-1">
-            <a class="text-dark" href="javascript:void(0)">
-              Starting your own home based online business from scratch
-            </a>
-          </h5>
-          <p class="fw-medium fs-sm text-muted">
-            <?php echo $cb->get_name(); ?> &middot; 24 min
-          </p>
-        </div>
-        <div class="block-content py-3 bg-body-light flex-grow-0">
-          <div class="row g-0 fs-sm text-center">
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="far fa-fw fa-eye opacity-50 me-1"></i> 1.5k
-              </a>
-            </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-heart opacity-50 me-1"></i> 300
-              </a>
-            </div>
-            <div class="col-4">
-              <a class="text-muted fw-semibold" href="javascript:void(0)">
-                <i class="fa fa-fw fa-comments opacity-50 me-1"></i> 750
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- END Story #1 -->
-    </div>
+    <?php }?>
   </div>
   <!-- END Experiencias y trayectoria -->
 
@@ -328,15 +320,33 @@ $cb->l_header_style = 'dark-glass';
     </div>
   </div>
 <?php } ?>
-<!-- jQuery (required for Magnific Popup plugin) -->
-<?php $cb->get_js('js/lib/jquery.min.js'); ?>
+<script >
+  document.querySelector('.play').onclick = player;
+  console.log(player);
 
-<!-- Page JS Plugins -->
-<?php $cb->get_js('js/plugins/magnific-popup/jquery.magnific-popup.min.js'); ?>
+  function player(event) {
+    const clicked = event.target;
+    console.log(clicked);
+    clicked.classList.toggle('playing');
+    const song = document.querySelector('.audio');
 
-<!-- Page JS Helpers (Magnific Popup Plugin) -->
-<script>Codebase.helpersOnLoad(['jq-magnific-popup']);</script>
+    if (song.paused || song.ended) {
+      song.play();
+    } else {
+      song.pause();
+    }
+  }
+</script>
+<script src="custom.js"></script>
 
 <?php require 'inc/_global/views/page_end.php'; ?>
 <?php require 'inc/_global/views/footer_start.php'; ?>
+
+<!-- Page JS Plugins -->
+<?php $cb->get_js('js/plugins/simplemde/simplemde.min.js'); ?>
+<?php $cb->get_js('js/plugins/ckeditor/ckeditor.js'); ?>
+
+<!-- Page JS Helpers (SimpleMDE + CKEditor plugins) -->
+<script>Codebase.helpersOnLoad(['js-ckeditor', 'js-simplemde']);</script>
+
 <?php require 'inc/_global/views/footer_end.php'; ?>
--- dashboard/php/sobremiController.php
+++ dashboard/php/sobremiController.php
@@ -45,4 +45,196 @@
         $conn = null;  
     }
 
+    // update update_info_sobre_mi
+    if(isset($_POST['update_info_sobre_mi'])){
+        $id_sobre_mi = 1;
+        $subtitulo = $_POST['subtitulo'];
+        $descripcion = $_POST['descripcion'];
+        
+        try {
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $resultado = "UPDATE sobre_mi SET subtitulo = '$subtitulo', descripcion = '$descripcion'
+                        WHERE id_sobre_mi = ".$id_sobre_mi;
+            
+            // Prepare statement
+            $stmt = $conn->prepare($resultado);
+            
+            // execute the query
+            $stmt->execute();
+            
+            // echo a message to say the UPDATE succeeded
+            // echo $stmt->rowCount() . " records UPDATED successfully";
+
+            $_SESSION['title'] = '¡Yupi! Todo ha salido bien';
+            $_SESSION['message'] = 'Se ha actualizado la información correctamente';
+            $_SESSION['icon'] = 'fa-solid fa-circle-check';
+            $_SESSION['message_type'] = 'success';
+            header('Location: ../bogar_sobre_mi.php#sobremi');
+        }catch(PDOException $e) {
+            echo $resultado . "<br>" . $e->getMessage();
+        }
+        $conn = null;  
+    }
+
+    // update foto_sobre_mi
+    if(isset($_POST['update_foto_sobre_mi'])){
+        $id_sobre_mi = 1;
+        $foto = $_FILES['foto']['name'];
+
+        if(isset($foto) && $foto != ""){
+            $tipo = $_FILES['foto']['type'];
+            $temp  = $_FILES['foto']['tmp_name'];
+    
+           if( !((strpos($tipo,'jpg') || strpos($tipo,'png') || strpos($tipo,'jpeg') || strpos($tipo,'webp')))){
+            $_SESSION['title'] = '¡Hey! Checa este mensaje';
+            $_SESSION['message'] = 'Solo se permite archivos jpeg, jpg, png, webp';
+            $_SESSION['icon'] = 'fa-solid fa-circle-exclamation';
+            $_SESSION['message_type'] = 'warning';
+            header('Location: ../bogar_sobre_mi.php#sobremi');
+           }else{
+            try {
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              
+                $resultado = "UPDATE sobre_mi SET foto = '$foto' WHERE id_sobre_mi = ".$id_sobre_mi;
+              
+                // Prepare statement
+                $stmt = $conn->prepare($resultado);
+              
+                // execute the query
+                $stmt->execute();
+              
+                // echo a message to say the UPDATE succeeded
+                echo $stmt->rowCount() . " records UPDATED successfully";
+              } catch(PDOException $e) {
+                echo $resultado . "<br>" . $e->getMessage();
+              }
+              
+              $conn = null;
+
+            //  $resultado = mysqli_query($connect,$query);
+             if($stmt){
+                move_uploaded_file($temp,'../assets/media/sobre_mi/'.$foto);   
+
+                $_SESSION['title'] = '¡Yupi! Todo ha salido bien';
+                $_SESSION['message'] = 'Se ha actualizado la imagen correctamente';
+                $_SESSION['icon'] = 'fa-solid fa-circle-check';
+                $_SESSION['message_type'] = 'success';
+
+                header('Location: ../bogar_sobre_mi.php#sobremi');
+             }else{
+                $_SESSION['title'] = '¡Ouch! Algo salio mal';
+                $_SESSION['message'] = 'No se pudo cargar tu imagen, hazlo de nuevo';
+                $_SESSION['icon'] = 'fa-solid fa-circle-xmark';
+                $_SESSION['message_type'] = 'danger';
+
+                header('Location: ../bogar_sobre_mi.php#sobremi');
+             }
+           }
+        }else{
+            $_SESSION['title'] = '¡Hey! Checa este mensaje';
+            $_SESSION['message'] = 'Debes cargar una imagen';
+            $_SESSION['icon'] = 'fa-solid fa-circle-exclamation';
+            $_SESSION['message_type'] = 'warning';
+            header('Location: ../bogar_sobre_mi.php#sobremi');
+        }
+    } 
+
+    // update update_foto_historia
+    if(isset($_POST['update_foto_historia'])){
+        $id_historia = 1;
+        $foto = $_FILES['foto']['name'];
+
+        if(isset($foto) && $foto != ""){
+            $tipo = $_FILES['foto']['type'];
+            $temp  = $_FILES['foto']['tmp_name'];
+    
+           if( !((strpos($tipo,'jpg') || strpos($tipo,'png') || strpos($tipo,'jpeg') || strpos($tipo,'webp')))){
+            $_SESSION['title'] = '¡Hey! Checa este mensaje';
+            $_SESSION['message'] = 'Solo se permite archivos jpeg, jpg, png, webp';
+            $_SESSION['icon'] = 'fa-solid fa-circle-exclamation';
+            $_SESSION['message_type'] = 'warning';
+            header('Location: ../bogar_sobre_mi.php#mihistoria');
+           }else{
+            try {
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              
+                $resultado = "UPDATE mi_historia SET foto = '$foto' WHERE id_historia = ".$id_historia;
+              
+                // Prepare statement
+                $stmt = $conn->prepare($resultado);
+              
+                // execute the query
+                $stmt->execute();
+              
+                // echo a message to say the UPDATE succeeded
+                echo $stmt->rowCount() . " records UPDATED successfully";
+              } catch(PDOException $e) {
+                echo $resultado . "<br>" . $e->getMessage();
+              }
+              
+              $conn = null;
+
+            //  $resultado = mysqli_query($connect,$query);
+             if($stmt){
+                move_uploaded_file($temp,'../assets/media/mi_historia/'.$foto);   
+
+                $_SESSION['title'] = '¡Yupi! Todo ha salido bien';
+                $_SESSION['message'] = 'Se ha actualizado la imagen correctamente';
+                $_SESSION['icon'] = 'fa-solid fa-circle-check';
+                $_SESSION['message_type'] = 'success';
+
+                header('Location: ../bogar_sobre_mi.php#mihistoria');
+             }else{
+                $_SESSION['title'] = '¡Ouch! Algo salio mal';
+                $_SESSION['message'] = 'No se pudo cargar tu imagen, hazlo de nuevo';
+                $_SESSION['icon'] = 'fa-solid fa-circle-xmark';
+                $_SESSION['message_type'] = 'danger';
+
+                header('Location: ../bogar_sobre_mi.php#mihistoria');
+             }
+           }
+        }else{
+            $_SESSION['title'] = '¡Hey! Checa este mensaje';
+            $_SESSION['message'] = 'Debes cargar una imagen';
+            $_SESSION['icon'] = 'fa-solid fa-circle-exclamation';
+            $_SESSION['message_type'] = 'warning';
+            header('Location: ../bogar_sobre_mi.php#mihistoria');
+        }
+    } 
+
+     // update update_mi_historia
+     if(isset($_POST['update_mi_historia'])){
+        $id_historia = 1;
+        $descripcion = $_POST['descripcion'];
+        
+        try {
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            $resultado = "UPDATE mi_historia SET descripcion = '$descripcion'
+                        WHERE id_historia = ".$id_historia;
+            
+            // Prepare statement
+            $stmt = $conn->prepare($resultado);
+            
+            // execute the query
+            $stmt->execute();
+            
+            // echo a message to say the UPDATE succeeded
+            // echo $stmt->rowCount() . " records UPDATED successfully";
+
+            $_SESSION['title'] = '¡Yupi! Todo ha salido bien';
+            $_SESSION['message'] = 'Se ha actualizado la información correctamente';
+            $_SESSION['icon'] = 'fa-solid fa-circle-check';
+            $_SESSION['message_type'] = 'success';
+            header('Location: ../bogar_sobre_mi.php#mihistoria');
+        }catch(PDOException $e) {
+            echo $resultado . "<br>" . $e->getMessage();
+        }
+        $conn = null;  
+    }
 ?>
